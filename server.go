01af5c0f3d71b56bb3c28c4ca9161b4670cedb81
dvaumoron <dvaumoron@gmail.com>
2023-08-02 16:10:05 +0200

    cleaning OPA modification & update dependencies

--- server.go
+++ server.go
@@ -19,31 +19,19 @@
 package main
 
 import (
-	"context"
 	_ "embed"
 
 	dbclient "github.com/dvaumoron/puzzledbclient"
 	grpcserver "github.com/dvaumoron/puzzlegrpcserver"
 	"github.com/dvaumoron/puzzlerightserver/rightserver"
 	pb "github.com/dvaumoron/puzzlerightservice"
-	"github.com/open-policy-agent/opa/sdk"
-	"go.uber.org/zap"
 )
 
 //go:embed version.txt
 var version string
 
 func main() {
-	// should start with this, to benefit from the call to godotenv
 	s := grpcserver.Make(rightserver.RightKey, version)
-
-	ctx := context.Background()
-	opa, err := sdk.New(ctx, sdk.Options{})
-	if err != nil {
-		s.Logger.FatalContext(ctx, "Failed to initialize OPA client", zap.Error(err))
-	}
-	defer opa.Stop(ctx)
-
 	pb.RegisterRightServer(s, rightserver.New(dbclient.Create(s.Logger), s.Logger))
 	s.Start()
 }
